4f8b8ee5bcc8e50faba88464dc0958803d58ad08
Igor Afonso Nunes <igorafonsonunes@outlook.com>
2021-02-14 19:36:46 -0300

    Criação da rota principal do projeto.

---
+++ app/routes/send-mail.routes.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import SendMailController from "../controllers/send-mail.controller";
+
+export default class SendEmailRoutes {
+  private controller: SendMailController;
+
+  constructor(){
+    this.controller = new SendMailController();
+  }
+
+  public init = (): Router => {
+    const router: Router = Router();
+    router.use('/', router);
+    router.post('/:api_key', this.controller.sendMail);
+    return router;
+  }
+}
